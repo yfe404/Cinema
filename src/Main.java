83d73b694d36cedfc1182ecae9e4bc126f4e0945
yafeunteun <yafeunteun@localhost.localdomain>
2014-02-20 19:11:28 +0100

    Ajout d'une classe de test en pour la gestion des films avec une base de données SQLITE3.wq

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,10 @@ public class Main {
 
 	public static void main(String[] args) {
 		
+		/* BEGIN TEST DATABASE */
+		SQLiteJDBC database = new SQLiteJDBC();
+		/* END TEST DATABASE */
+		
 		CSVFilmParser parser = new CSVFilmParser("cinedome_20140128.csv");
 		Cinema c = new Cinema(parser.parseFile());
 		AfficheurGui a = new AfficheurGui("Cinedome", c);
